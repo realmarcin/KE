e4076f838ea45c69c05ac813d8a152266dd410f6
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-05-20 10:43:00 -0700

    adding Shapiro normality

---
+++ src/dataclean/TableSummaries.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import shapiro
+
+
+
+
+def shapiro_columns(df, progress = False):
+
+    shapiro_out = []
+
+    count = 0
+    for col in df.columns:
+
+        stat, p = shapiro(df[col])
+
+        if (progress & count % 100 == 0):
+            print("count " + str(count))
+            print("stat, p %s %s" % (stat, p))
+            print(df[col])
+
+        count = count + 1
+        shapiro_out.append([stat,p])
+
+    shapiro_out_df = pd.DataFrame(shapiro_out, columns=["stat", "p"], dtype=np.float64)
+    if (progress):
+        print("done shapiro, shape")
+        print(shapiro_out_df.shape)
+    shapiro_out_df
